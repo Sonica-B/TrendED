823ef51fba9f4cf9d35c5b04517a814bd8d144e5
Harrison Taylor <htaylor1769@gmail.com>
2024-12-02 19:51:39 -0500

    Resume_parser

---
+++ backend/resume_parser.py
@@ -0,0 +1,24 @@
+import json
+import pandas as pd
+from docx import Document
+
+
+def extract_all_text(doc_path):
+    doc = Document(doc_path)
+    all_text = []
+
+    for para in doc.paragraphs:
+        text = para.text.strip()
+        if text:
+            all_text.append(text)
+
+    return {"all_text": all_text}
+
+
+def save_to_files(data, json_path):
+    with open(json_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+data = extract_all_text("../data/resume.docx")
+save_to_files(data, "../data/resume.json")
